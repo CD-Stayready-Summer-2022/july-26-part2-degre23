f6e6b635d293aaaf36b49e677d71ea0f47bedcfa
degre23 <98403668+degre23@users.noreply.github.com>
2022-07-26 10:55:02 -0400

    july26-2

--- src/main/java/cd/get/ready/algorithms/CountDuplicates.java
+++ src/main/java/cd/get/ready/algorithms/CountDuplicates.java
@@ -1,5 +1,8 @@
 package cd.get.ready.algorithms;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class CountDuplicates {
     /**
      * Example of overloading a function that accepts a Array String
@@ -7,6 +10,13 @@ public class CountDuplicates {
      * @return
      */
     public Integer countDuplicates(String[] input){
+        Map<String, Integer> countDupes = new HashMap<String, Integer>();
+        int count = 0;
+
+
+        for (int i = 0; i < input.length; i++){
+            count <=  ;
+        }
         return null;
     }
 
